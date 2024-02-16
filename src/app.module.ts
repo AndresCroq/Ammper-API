3ec7936b0742b68efc9e61e8458bfb45ae54ccd2
unknown <devandrescroquer@gmail.com>
2024-02-16 15:16:36 -0300

    feat: database configured

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,21 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BankModule } from './bank/bank.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [BankModule],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        uri: config.get<string>('DB'),
+      }),
+    }),
+    BankModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
