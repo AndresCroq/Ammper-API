fb7d3f718844e367f9b928cebb4868b22d98ead4
Andres Croquer <devandrescroquer@gmail.com>
2024-02-17 16:44:37 -0300

    feat: table and scatter formats

--- src/bank/formatter.service.ts
+++ src/bank/formatter.service.ts
@@ -2,7 +2,8 @@ import { BadRequestException, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Bank } from './schemas/bank.schema';
 import { Model } from 'mongoose';
-import { Bar } from './interfaces';
+import { Bar, ScatterVector, scatterSeries } from './interfaces';
+import { BankTable } from './interfaces/raw.interface';
 
 @Injectable()
 export class FormatterService {
@@ -14,12 +15,54 @@ export class FormatterService {
     switch (method) {
       case 'bar':
         return this.bar(data, flow);
+      case 'scatter':
+        return this.scatter(data, flow);
+      case 'custom':
+        return new Array(...new Set(data.map((e) => e.account.category)));
+      case 'table':
+        return this.table(data);
       case 'raw':
-        // return new Array(...new Set(data.map((e) => e.account.type)));
         return data;
       default:
         throw new BadRequestException('Wrong formatting method.');
     }
+  }
+
+  private table(data: Bank[]): BankTable[] {
+    const tableValues: BankTable[] = data.map((e) => ({
+      category: e.category ? e.category : 'Others',
+      accountCategory: e.account.category,
+      merchantName: e.merchant.name,
+      amount: e.amount,
+      type: e.type,
+      status: e.status,
+      balance: e.balance,
+      valueDate: e.value_date,
+    }));
+
+    return tableValues;
+  }
+
+  private scatter(data: Bank[], flow: string) {
+    const names = new Array(...new Set(data.map((e) => e.category)));
+    const series = this.scatterSeries(names);
+
+    names.forEach((category) => {
+      const temp: ScatterVector[] = [];
+      data.forEach((e) => {
+        if (e.category === category && e.type === flow) {
+          temp.push([e.value_date, e.amount]);
+        }
+      });
+
+      const filtered = series.filter((e) =>
+        category ? e.name === category : e.name === 'Others',
+      )[0];
+
+      filtered.data = temp;
+    });
+
+    return series;
   }
 
   private bar(data: Bank[], flow = 'OUTFLOW'): Bar {
@@ -28,7 +71,7 @@ export class FormatterService {
     const series = names.map((name) => {
       const amountsByIndex = new Array(dates.length).fill(0);
       data
-        .filter((e) => e.category === name && e.type === flow)
+        .filter((e) => e.category === name && e.type === flow && e.amount > 0)
         .map(({ value_date, amount }) => {
           const dateIndex = dates.indexOf(value_date);
           amountsByIndex[dateIndex] = amountsByIndex[dateIndex] += amount * 100;
@@ -36,7 +79,7 @@ export class FormatterService {
 
       const amountFloats = amountsByIndex.map((e) => (e > 0 ? e / 100 : e));
       return {
-        name,
+        name: name ? name : 'Others',
         data: amountFloats,
       };
     });
@@ -66,127 +109,32 @@ export class FormatterService {
 
     return bar;
   }
+
+  private scatterSeries(categories: string[]): scatterSeries[] {
+    const symbols = [
+      'Circle',
+      'Square',
+      'Diamond',
+      'Triangle',
+      'Triangle-down',
+      'Hexagon',
+      'Octagon',
+      'Star',
+      'Cross',
+      'X-mark',
+    ];
+
+    const data = categories.map((category) => {
+      const symbol = Math.floor(Math.random() * symbols.length);
+      return {
+        name: category ? category : 'Others',
+        id: category ? category.toLowerCase() : 'others',
+        marker: {
+          symbol: symbols[symbol],
+        },
+      };
+    });
+
+    return data;
+  }
 }
-
-// const series = [
-//   {
-//     name: 'Basketball',
-//     id: 'basketball',
-//     marker: {
-//       symbol: 'circle',
-//     },
-//   },
-//   {
-//     name: 'Triathlon',
-//     id: 'triathlon',
-//     marker: {
-//       symbol: 'triangle',
-//     },
-//   },
-//   {
-//     name: 'Volleyball',
-//     id: 'volleyball',
-//     marker: {
-//       symbol: 'square',
-//     },
-//   },
-// ];
-
-// async function getData() {
-//   const response = await fetch(
-//     'https://www.highcharts.com/samples/data/olympic2012.json',
-//   );
-//   return response.json();
-// }
-
-// Highcharts.setOptions({
-//   colors: ['rgba(5,141,199,0.5)', 'rgba(80,180,50,0.5)', 'rgba(237,86,27,0.5)']
-// });
-
-// Los gastos son los colores (sports)
-// X axis serían las fechas (height)
-// Y axis sería la cantidad gastada (weight)
-// Sí, Jalu. Yo sé que puse que el X axis es height,
-// pero en el gráfico de ejemplo está así.
-// A mí también me da TOC, no es mi culpa.
-
-// getData().then(data => {
-//   const getData = sportName => {
-//       const temp = [];
-//       data.forEach(elm => {
-//           if (elm.sport === sportName && elm.weight > 0 && elm.height > 0) {
-//               temp.push([elm.height, elm.weight]);
-//           }
-//       });
-//       return temp;
-//   };
-//   series.forEach(s => {
-//       s.data = getData(s.id);
-//   });
-
-//   Highcharts.chart('container', {
-//       chart: {
-//           type: 'scatter',
-//           zoomType: 'xy'
-//       },
-//       title: {
-//           text: 'Olympics athletes by height and weight',
-//           align: 'left'
-//       },
-//       subtitle: {
-//           text:
-//         'Source: <a href="https://www.theguardian.com/sport/datablog/2012/aug/07/olympics-2012-athletes-age-weight-height">The Guardian</a>',
-//           align: 'left'
-//       },
-//       xAxis: {
-//           title: {
-//               text: 'Height'
-//           },
-//           labels: {
-//               format: '{value} m'
-//           },
-//           startOnTick: true,
-//           endOnTick: true,
-//           showLastLabel: true
-//       },
-//       yAxis: {
-//           title: {
-//               text: 'Weight'
-//           },
-//           labels: {
-//               format: '{value} kg'
-//           }
-//       },
-//       legend: {
-//           enabled: true
-//       },
-//       plotOptions: {
-//           scatter: {
-//               marker: {
-//                   radius: 2.5,
-//                   symbol: 'circle',
-//                   states: {
-//                       hover: {
-//                           enabled: true,
-//                           lineColor: 'rgb(100,100,100)'
-//                       }
-//                   }
-//               },
-//               states: {
-//                   hover: {
-//                       marker: {
-//                           enabled: false
-//                       }
-//                   }
-//               },
-//               jitter: {
-//                   x: 0.005
-//               }
-//           }
-//       },
-//       tooltip: {
-//           pointFormat: 'Height: {point.x} m <br/> Weight: {point.y} kg'
-//       },
-//       series
-//   });
-// }
